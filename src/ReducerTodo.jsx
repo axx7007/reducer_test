79dd8c54d65948d73e4bdc411693e360c9281727
abduvaxobovxojiakbar7007 <xojiakbarabduvaxobov7007@gmail.com>
2022-04-11 18:35:43 +0500

    changed

--- src/ReducerTodo.jsx
+++ src/ReducerTodo.jsx
@@ -10,6 +10,18 @@ const ReducerTodo = () => {
           ...state,
           { id: Date.now(), name: action.payload.name, completed: false },
         ];
+      case "delete":
+        return state.filter((value) => value.id !== action.payload.id);
+      case "toggle":
+        state.map((item) => {
+          if (item.id === action.payload.id) {
+            return { ...item, completed: !item.completed };
+          }
+          return item;
+        });
+
+      default:
+        return state;
     }
   };
   const [todos, dispatch] = useReducer(reducer, []);
@@ -30,7 +42,7 @@ const ReducerTodo = () => {
       <button onClick={onAdd}>Add</button>
 
       {todos.map((value) => {
-        return <Todos value={value} />;
+        return <Todos value={value} dispatch={dispatch} />;
       })}
     </div>
   );
--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-export const Todos = ({ value }) => {
+export const Todos = ({ value, dispatch }) => {
   return (
     <div>
-      <h1>{value.name}</h1>
-      <button>Delete</button>
+      <h1 style={{ color: value.completed ? "red" : "black" }}>{value.name}</h1>
+      <button
+        onClick={() => dispatch({ type: "toggle", payload: { id: value.id } })}
+      >
+        Toggle
+      </button>
+      <button
+        onClick={() => dispatch({ type: "delete", payload: { id: value.id } })}
+      >
+        Delete
+      </button>
     </div>
   );
 };
