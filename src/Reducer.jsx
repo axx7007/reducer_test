2a608f03597c53e2fff5f7da8ef1d14725344f04
abduvaxobovxojiakbar7007 <xojiakbarabduvaxobov7007@gmail.com>
2022-04-11 13:57:04 +0500

    payload tested

--- src/Reducer.jsx
+++ src/Reducer.jsx
@@ -2,14 +2,45 @@ import React, { useReducer, useState } from "react";
 
 export const Reducer = () => {
   //   const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer((state, action) => {
-    return 10 - state;
-  }, 1);
+  const [value, setValue] = useState(1);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "minus":
+        return state - 1;
+      case "plus":
+        return state + 1;
+      case "byamount":
+        return state + action.payload.amount;
+      default:
+        return state;
+    }
+  };
+  const [count, dispatch] = useReducer(reducer, 0);
+
+  const EditValue = (e) => {
+    var variable = parseInt(e.target.value);
+    console.log(typeof variable);
+    setValue(variable);
+  };
   return (
     <div>
       <h1>Count{count}</h1>
-      <button onClick={() => dispatch()}>+</button>
-      <button onClick={() => dispatch()}>-</button>
+      <button onClick={() => dispatch({ type: "plus" })}>+</button>
+      <button onClick={() => dispatch({ type: "minus" })}>-</button>
+      <button
+        onClick={() =>
+          dispatch({ type: "byamount", payload: { amount: value } })
+        }
+      >
+        {value}
+      </button>
+      <select onChange={EditValue}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
     </div>
   );
 };
